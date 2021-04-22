87569a64ee77a170c132fb863c682f5c8125e97d
Raul Viana <raulmanuelviana@gmail.com>
2021-04-22 15:37:18 +0100

    fix in db script

--- database/create.sql
+++ database/create.sql
@@ -1,7 +1,9 @@
 CREATE TABLE users (username CHAR[8] NOT NULL PRIMARY KEY, 
                     password_hash TEXT,
                     security_level INT CHECK(3 >= security_level >= 1),
-                    one_time_id ,
-                    ip_address );
+                    one_time_id TEXT,
+                    ip_address TEXT,
+                    privat_key TEXT 
+            );
 
-INSERT INTO TABLE users VALUES ("Pedro", "this_should_be_a_hash", 3, "another_hash", "127.0.0.1:3000");
+INSERT INTO users VALUES ("Pedro", "this_should_be_a_hash", 3, "another_hash", "127.0.0.1:3000");
