2a4abe1ef38a3fd3e7d2a4cb128b279a4ff07527
Raul Viana <raulmanuelviana@gmail.com>
2021-04-22 21:28:14 +0100

    typo in database script

--- database/create.sql
+++ database/create.sql
@@ -3,7 +3,7 @@ CREATE TABLE users (username CHAR[8] NOT NULL PRIMARY KEY,
                     security_level INT CHECK(3 >= security_level >= 1),
                     one_time_id TEXT,
                     ip_address TEXT,
-                    privat_key TEXT 
+                    public_key TEXT 
             );
 
-INSERT INTO users VALUES ("Pedro", "this_should_be_a_hash", 3, "another_hash", "127.0.0.1:3000");
+INSERT INTO users VALUES ("Pedro", "this_should_be_a_hash", 3, "another_hash", "127.0.0.1:3000", "rrrrrrrrrrrrrrrrrr");
