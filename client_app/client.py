54431640527d5cfec7fe28ffbacc6764a56db939
Raul Viana <raulmanuelviana@gmail.com>
2021-04-26 17:14:52 +0100

    finnaly working

--- client_app/client.py
+++ client_app/client.py
@@ -1,4 +1,19 @@
+
+import base64
+from cryptography.hazmat.backends.interfaces import RSABackend
 import requests
-response = requests.get("http://127.0.0.1:3000/auth")
-print(response)
-print(response.json())  # This method is convenient when the API returns JSON
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.PublicKey import RSA
+
+
+ID = str.encode("123456")
+# First-Registration
+# response = requests.get("http://127.0.0.1:3000/register")
+# open('server_public.pem', 'wb').write(response.content)
+token = '1234'
+
+key = RSA.importKey(open('server_public.pem').read())
+cipher = PKCS1_OAEP.new(key)
+output = base64.b64encode(cipher.encrypt(token.encode('utf-8')))
+token = requests.post(
+    "http://127.0.0.1:3000/register/get_token", json={'token': output})
