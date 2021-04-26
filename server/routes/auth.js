2a2cfdbf6f43e3aecec35c24cff4b88c59030857
Raul Viana <raulmanuelviana@gmail.com>
2021-04-26 16:40:12 +0100

    shit :(

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,7 +3,6 @@ let app = express.Router()
 
 app.get('/', function (req, res) {
     console.log(req.body);
-    res.json({"server_public_key": 'sign In'});
 });
 
 module.exports = app
--- server/routes/register.js
+++ server/routes/register.js
@@ -1,8 +1,27 @@
-const express = require('express')
+const fs = require('fs');
+const forge = require('node-forge');
+const express = require('express');
 let app = express.Router()
 
 app.get('/', function (req, res) {
-    res.send('register');
+    console.log(req.body);
+    res.download('../server/public.pem');
+});
+
+app.post('/get_token', function (req, res) {
+    console.log(req.body.token);
+    const pem = fs.readFileSync('../server/private.pem', 'utf8');
+    const privateKey = forge.pki.decryptRsaPrivateKey(pem, '2210');
+    privateKey = forge.pki.privateKeyFromPem(privateKey);
+    
+    const token = privateKey.decrypt(forge.util.decode64(req.body.token), 'RSA-OAEP', {
+        md: forge.md.sha1.create(),
+        mgf1: {
+        md: forge.md.sha1.create()
+        }
+    });
+    console.log(decrypted_token);
+    res.json({'ok':'ok'});
 });
 
 module.exports = app
