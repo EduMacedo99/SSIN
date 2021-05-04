a5414b676eb114d07332910b541b0199da9fc7e5
Raul Viana <raulmanuelviana@gmail.com>
2021-05-04 16:38:57 +0100

    simmetric encrypt decrypt almost ready

--- client_app/client.py
+++ client_app/client.py
@@ -1,16 +1,30 @@
-
 import requests
-import encrypt
+import simmetric_encryption
+import os, binascii
+import base64
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.PublicKey import RSA
 
-SERVER_KEY_PATH = 'server_public.pem'
+
+SERVER_KEY_PATH = "server_public.pem"
+SERVER_URL = "http://127.0.0.1:3000/"
 one_time_ID = "123456"
 # First-Registration
 # get server public key
 # response = requests.get("http://127.0.0.1:3000/register")
-# open('server_public.pem', 'wb').write(response.content)
+# open('SERVER_KEY_PATH', 'wb').write(response.content)
 
-message = encrypt.encrypt(SERVER_KEY_PATH, one_time_ID)
-token = requests.post(
-    "http://127.0.0.1:3000/register/get_token", json={'token': message})
+key = RSA.importKey(open(SERVER_KEY_PATH).read())
+cipher = PKCS1_OAEP.new(key)
+one_time_ID_encrypt = base64.b64encode(cipher.encrypt(one_time_ID.encode("utf-8")))
 
+encrypt_password = binascii.b2a_hex(os.urandom(12))
 
+token_encrypt = requests.post(
+    SERVER_URL + "register/get_token",
+    json={"token": one_time_ID_encrypt, "encrypt_pass": encrypt_password},
+).json()
+
+token = simmetric_encryption.AESCipher(encrypt_password).decrypt(token_encrypt["token"])
+print(token)
+# print(token)
--- server/routes/register.js
+++ server/routes/register.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const forge = require('node-forge');
 const express = require('express');
+const Crypto = require('crypto');
 let app = express.Router()
 
 app.get('/', function (req, res) {
@@ -13,14 +14,51 @@ app.post('/get_token', function (req, res) {
     const pem = fs.readFileSync('../server/private.pem', 'utf8');
     const privateKey = forge.pki.decryptRsaPrivateKey(pem, '2210');
     
-    const token = privateKey.decrypt(forge.util.decode64(req.body.token), 'RSA-OAEP', {
+    const onetimeID = privateKey.decrypt(forge.util.decode64(req.body.token), 'RSA-OAEP', {
         md: forge.md.sha1.create(),
         mgf1: {
-        md: forge.md.sha1.create()
+            md: forge.md.sha1.create()
         }
     });
-    console.log(token);
-    res.json({'ok':'ok'});
+    console.log(onetimeID);
+    console.log(req.body.encrypt_pass);
+    //confirmar na BD que cliente onetimeID é correto
+    //criar um token
+    const token = Crypto.randomBytes(12).toString('base64').slice(0, 12);
+    //guardar na DB
+    //encriptar com chave simétrica -> append 16 bytes to IV
+    
+    //enviar token para o cliente
+    res.json({'token': token});
 });
 
 module.exports = app
+
+//encrypt and decrypt with AES-CBC
+
+// 'use strict';
+
+// const crypto = require('crypto');
+// const ENC_KEY = "bf3c199c2470cb477d907b1e0917c17b"; // set random encryption key
+// const IV = "5183666c72eec9e4"; // set random initialisation vector
+// // ENC_KEY and IV can be generated as crypto.randomBytes(32).toString('hex');
+
+// const phrase = "who let the dogs out";
+
+// var encrypt = ((val) => {
+//   let cipher = crypto.createCipheriv('aes-256-cbc', ENC_KEY, IV);
+//   let encrypted = cipher.update(val, 'utf8', 'base64');
+//   encrypted += cipher.final('base64');
+//   return encrypted;
+// });
+
+// var decrypt = ((encrypted) => {
+//   let decipher = crypto.createDecipheriv('aes-256-cbc', ENC_KEY, IV);
+//   let decrypted = decipher.update(encrypted, 'base64', 'utf8');
+//   return (decrypted + decipher.final('utf8'));
+// });
+
+
+
+// encrypted_key = encrypt(phrase);
+// original_phrase = decrypt(encrypted_key);
