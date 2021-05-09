62f2944c57ea1819fbd4627aebbfc70e7b2b0e7d
Raul Viana <raulmanuelviana@gmail.com>
2021-05-09 21:12:25 +0100

    simmetric key encryption and decryption done

--- server/routes/register.js
+++ server/routes/register.js
@@ -4,13 +4,25 @@ const express = require('express');
 const Crypto = require('crypto');
 let app = express.Router()
 
+const crypto = require('crypto') 
+const buffertrim = require('buffertrim') 
+const simmetric = require('../simmetric_encryption')
+
 app.get('/', function (req, res) {
     console.log(req.body);
     res.download('../server/public.pem');
 });
 
 app.post('/get_token', function (req, res) {
+    const token = "333333333333iiiiiiiiiiiiiiiiiiiiiii3333333333"
+    const iv = "aaaaaaaaaaaaaaaa"
+    const key = "aaaaaaaaaaaaaaaa"
+ 
     console.log(req.body.token);
+    console.log(req.body.encrypt_pass)
+    console.log(req.body.cenas)
+    const cenas_dec = simmetric.decrypt(req.body.cenas, iv, key)
+    console.log("decript: "+ cenas_dec)
     const pem = fs.readFileSync('../server/private.pem', 'utf8');
     const privateKey = forge.pki.decryptRsaPrivateKey(pem, '2210');
     
@@ -20,45 +32,27 @@ app.post('/get_token', function (req, res) {
             md: forge.md.sha1.create()
         }
     });
-    console.log(onetimeID);
-    console.log(req.body.encrypt_pass);
+    const simmetric_key = privateKey.decrypt(forge.util.decode64(req.body.encrypt_pass), 'RSA-OAEP', {
+        md: forge.md.sha1.create(),
+        mgf1: {
+            md: forge.md.sha1.create()
+        }
+    });
+    console.log("ID: " + onetimeID);
+    console.log("pass: " + simmetric_key);
     //confirmar na BD que cliente onetimeID é correto
     //criar um token
-    const token = Crypto.randomBytes(12).toString('base64').slice(0, 12);
-    //guardar na DB
-    //encriptar com chave simétrica -> append 16 bytes to IV
+    //const token = Crypto.randomBytes(12).toString('base64').slice(0, 12);
     
-    //enviar token para o cliente
-    res.json({'token': token});
+    //**************************************************************************** */
+    
+    
+    const enc_token = simmetric.encrypt(token, iv, key)
+  
+    console.log(token + " *********  " + enc_token)
+    
+    res.json({'token': enc_token, 'encrypt_pass':"sds"});
+
 });
 
 module.exports = app
-
-//encrypt and decrypt with AES-CBC
-
-// 'use strict';
-
-// const crypto = require('crypto');
-// const ENC_KEY = "bf3c199c2470cb477d907b1e0917c17b"; // set random encryption key
-// const IV = "5183666c72eec9e4"; // set random initialisation vector
-// // ENC_KEY and IV can be generated as crypto.randomBytes(32).toString('hex');
-
-// const phrase = "who let the dogs out";
-
-// var encrypt = ((val) => {
-//   let cipher = crypto.createCipheriv('aes-256-cbc', ENC_KEY, IV);
-//   let encrypted = cipher.update(val, 'utf8', 'base64');
-//   encrypted += cipher.final('base64');
-//   return encrypted;
-// });
-
-// var decrypt = ((encrypted) => {
-//   let decipher = crypto.createDecipheriv('aes-256-cbc', ENC_KEY, IV);
-//   let decrypted = decipher.update(encrypted, 'base64', 'utf8');
-//   return (decrypted + decipher.final('utf8'));
-// });
-
-
-
-// encrypted_key = encrypt(phrase);
-// original_phrase = decrypt(encrypted_key);
