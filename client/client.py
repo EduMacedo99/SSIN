cd0f5be3760ab9bd34f783eafef006e6dc9cda83
Eduardo Macedo <eduardojcmacedo@gmail.com>
2021-05-18 16:10:24 +0100

    QOL changes

--- client/client.py
+++ client/client.py
@@ -19,6 +19,8 @@ def registration():
 
     if success:
 
+        print('\nServer Registration was successfull!')
+
         #polos a escolher uma password
         while counter_pw < 3:
             #Ask for strong password
@@ -48,7 +50,7 @@ def registration():
 
         config = dotenv_values(".env")
 
-        #por info
+        #Colocar info no .env
         dotenv.set_key(dotenv_file, "USERNAME", username)
         dotenv.set_key(dotenv_file, "ID", ID)
         
@@ -79,8 +81,9 @@ counter = 0
 #ver se .env.aes existe - se não existir é pq nao houve registo
 if path.exists(".env.aes"):
     
+    print('> Already Registered\n> Proceeding with authentication')
+
     while counter < 3:
-        print('> Already Registered\n> Proceeding with authentication')
         
         #ask for password
         username = input('Username: ')
@@ -93,7 +96,9 @@ if path.exists(".env.aes"):
         except ValueError:
             print('Wrong username/password (or file is corrupted).')
             counter += 1
-            if counter >= 3: exit()
+            if counter >= 3: 
+                print('Number of tries exceeded. Terminating program...')
+                exit()
 
     #get info
     try:
